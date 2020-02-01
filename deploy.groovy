d5baa79a3c7810459ef5e2df93045c0a6ea6f872
michalnagosa2 <41059985+michalnagosa2@users.noreply.github.com>
2020-02-02 01:26:39 +0200

    last change

--- deploy.groovy
+++ deploy.groovy
@@ -1,9 +1,36 @@
 node('linux'){
-enviroment{
-	credentialsID = 'jenkins'
+def customImage = ""
+environment{
+    registry = 'michalnagosa96/opsschool-repo'
+    registryCredential = 'docker.michal,nagosa96'
+    customImage = ''
 }
 stage('git'){
-	git branch: 'master',
-	   url:'https://github.com/michalnagosa2/flask-http.git'
+git branch: 'master',
+url:'https://github.com/michalnagosa2/flask-http.git'
+}
+stage("create dockerfile") {
+sh """
+FROM ubuntu:14.04
+MAINTAINER Docker Education Team <education@docker.com>
+RUN apt-get update
+RUN DEBIAN_FRONTEND=noninteractive apt-get install -y -q python-all python-pip 
+ADD ./webapp/requirements.txt /tmp/requirements.txt
+RUN pip install -qr /tmp/requirements.txt
+ADD ./webapp /opt/webapp/
+WORKDIR /opt/webapp
+EXPOSE 5000
+CMD ["python", "app.py"]
+EOF
+"""
+}
+stage('Build image') {
+customImage = docker.build('michalnagosa96/opsschool-repo:tag')
+}
+stage("Push image") {
+docker.withRegistry('https://registry-1.docker.io/v1', 'docker.michal,nagosa96') {
+customImage.push()
 }
 }
+}
+	
